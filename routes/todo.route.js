fa4a576bea5e3e42ba4d18da68d92ad898862b85
sksamiii <128906151+sksamiii@users.noreply.github.com>
2024-11-29 20:23:43 +0530

    first commit

---
+++ routes/todo.route.js
@@ -0,0 +1,21 @@
+const express=require("express");
+const router = express.Router();
+const List=require("../model/todo.model");
+const User=require("../model/user.model");
+
+router.post("/add",async(req,res)=>{
+    try{
+        const newList=new List({
+            text:req.body.text,
+            user:req.body.user,
+        });
+        const list= await newList.save();
+        console.log(list);
+        res.status(201).json(list);
+    }catch(err){
+        res.status(400).send("Error"+err);
+    }
+    res.end();
+})
+
+module.exports=router;
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const mongoose  = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = 5002;
+
+app.use(express.json())
+app.use(cors());
+
+mongoose.connect(
+    "mongodb+srv://sksami853:Sksami020304!@cluster3.e5c18.mongodb.net/?retryWrites=true&w=majority&appName=Cluster3",
+ {}
+ )
+ .then(()=>{
+    console.log("Connected TO Database")
+ });
+ const userRoute=require("./routes/user.route");
+ const todoRoute=require("./routes/todo.route")
+ app.use("/user",userRoute);
+ app.use("/todo",todoRoute);
+
+
+app.get("/",(req, res)=>{
+    res.send("Hello World");
+    res.end();
+})
+app.listen(PORT,()=>{
+    console.log(`Server is running on the port http://localhost:${PORT}/`);
+})
